bad19ac8df38257629817465e1095255a94241cf
lia-oliveira <dev.gillian@gmail.com>
2025-04-15 19:48:00 -0300

    Migration v3 categoria_id add

--- sistemaevento/sistemaevento/src/main/resources/data.sql
+++ sistemaevento/sistemaevento/src/main/resources/data.sql
@@ -4,5 +4,5 @@ INSERT INTO tb_categoria(descricao) VALUES ('Workshop');
 INSERT INTO tb_categoria(descricao) VALUES ('Minicurso');
 INSERT INTO tb_categoria(descricao) VALUES ('Palestra');
 
-INSERT INTO tb_atividade(nome, descricao, preco) VALUES ('Curso de HTML', 'Aprenda HTML de forma prática', 80.00);
-INSERT INTO tb_atividade(nome, descricao, preco) VALUES ('Oficina de GitHub', 'Controle Versões de seus projetos', 50.00);
+INSERT INTO tb_atividade(nome, descricao, preco, categoria_id) VALUES ('Curso de HTML', 'Aprenda HTML de forma prática', 80.00, 1);
+INSERT INTO tb_atividade(nome, descricao, preco, categoria_id) VALUES ('Oficina de GitHub', 'Controle Versões de seus projetos', 50.00, 2);
